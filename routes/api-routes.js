eb129028ff078e3c9f7b7ac50c17ff409f08b4c5
Jason Schmitt <schmitty890@gmail.com>
2018-05-24 14:16:40 -0400

    creating api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,61 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var db = require("../models");
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the todos
+  app.get("/api/todos", function(req, res) {
+    //find all entries for a table when used with no options
+    db.Todo.findAll({}).then(function(result) {
+      //we have access to the todos as an argument inside of the cb
+      res.json(result);
+    });
+  });
+
+  // POST route for saving a new todo. We can create a todo using the data on req.body
+  app.post("/api/todos", function(req, res) {
+    db.Todo.create({
+      text: req.body.text,
+      complete: req.body.complete,
+      name: req.body.name
+    }).then(function(result) {
+      // `result` here would be the newly created todo
+      res.json(result);
+    });
+  });
+
+  // DELETE route for deleting todos. We can get the id of the todo to be deleted
+  // from req.params.id
+  app.delete("/api/todos/:id", function(req, res) {
+    // Destroy takes in one argument: a "where object describing the todos we want to destroy
+    db.Todo.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+
+  // PUT route for updating todos. We can access the updated todo in req.body
+  app.put("/api/todos", function(req, res) {
+    // Update takes in two arguments, an object describing the properties we want to update,
+    // and another "where" object describing the todos we want to update
+    db.Todo.update({
+      text: req.body.text,
+      complete: req.body.complete
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+};
